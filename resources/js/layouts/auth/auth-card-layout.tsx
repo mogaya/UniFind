f16fd270d6b2872e9e2021c38c4e387b3a475e1b
mogaya <mogayainnocent@gmail.com>
2025-09-02 09:45:02 +0300

    fix: Update AuthLayout to use AuthCardLayout

--- resources/js/layouts/auth/auth-card-layout.tsx
+++ resources/js/layouts/auth/auth-card-layout.tsx
@@ -1,4 +1,3 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
@@ -17,17 +16,17 @@ export default function AuthCardLayout({
             <div className="flex w-full max-w-md flex-col gap-6">
                 <Link href={route('home')} className="flex items-center gap-2 self-center font-medium">
                     <div className="flex h-9 w-9 items-center justify-center">
-                        <AppLogoIcon className="size-9 fill-current text-black dark:text-white" />
+                        <h1 className="text-2xl font-bold">UniFind</h1>
                     </div>
                 </Link>
 
-                <div className="flex flex-col gap-6">
-                    <Card className="rounded-xl">
-                        <CardHeader className="px-10 pt-8 pb-0 text-center">
+                <div className="flex flex-col gap-4">
+                    <Card className="gap-2 rounded-xl">
+                        <CardHeader className="px-10 pb-0 text-center">
                             <CardTitle className="text-xl">{title}</CardTitle>
                             <CardDescription>{description}</CardDescription>
                         </CardHeader>
-                        <CardContent className="px-10 py-8">{children}</CardContent>
+                        <CardContent className="px-10 py-4">{children}</CardContent>
                     </Card>
                 </div>
             </div>
