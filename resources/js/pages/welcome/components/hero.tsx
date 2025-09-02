2847a4b842b0e7a9bb3421869d8b94e1d0ddd6a4
mogaya <mogayainnocent@gmail.com>
2025-09-02 09:47:02 +0300

    feat: Add Hero component for lost and found item reporting

---
+++ resources/js/pages/welcome/components/hero.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@/components/ui/button';
+import Slider from 'react-slick';
+import hero1 from '../assets/hero1.jpg';
+import hero2 from '../assets/hero2.jpg';
+import hero3 from '../assets/hero3.jpg';
+import hero4 from '../assets/hero4.jpeg';
+
+const sliderImages = [hero1, hero2, hero3, hero4];
+
+const Hero = () => {
+    const settings = {
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        arrows: false,
+        pauseOnHover: false,
+    };
+
+    return (
+        <section className="relative h-[80vh] overflow-hidden md:h-[80vh]">
+            {/* Carousel Background */}
+            <div className="absolute inset-0 z-0">
+                <Slider {...settings}>
+                    {sliderImages.map((img, index) => (
+                        <img key={index} src={img} className="h-[80vh] w-full object-cover md:h-[90vh]" />
+                    ))}
+                </Slider>
+            </div>
+
+            {/* Overlay filter */}
+            <div className="absolute inset-0 z-10 bg-black/60" />
+
+            {/* Overlay Content */}
+            <div className="absolute inset-0 z-20 flex items-center justify-center px-4">
+                <div className="flex w-full max-w-3xl min-w-[250px] flex-col items-center space-y-4 text-center text-white">
+                    <h1 className="text-4xl font-bold md:text-5xl">Lost something?</h1>
+                    <h1 className="text-4xl font-bold md:text-5xl">We'll help you find it.</h1>
+                    <p className="text-md md:text-lg">
+                        Connect with your campus community to recover lost items and help others find what they've misplaced.
+                    </p>
+                    <div className="flex gap-4">
+                        <Button size="lg" className="hover:bg-navy bg-primary text-white">
+                            Report Lost Item
+                        </Button>
+                        <Button size="lg" className="hover:bg-navy hidden bg-primary text-white md:flex">
+                            Report Found Item
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
