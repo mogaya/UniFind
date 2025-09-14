bf1cf8702a12713dca90b357853f4e4a59ac5fc9
mogaya <mogayainnocent@gmail.com>
2025-09-14 23:27:37 +0300

    update lost items routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\LostItemController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-    // "/report-lost"
-    // "/report-found"
-    // "/found-items"
-    // "/my-reports"
+// "/report-found"
+// "/found-items"
+// "/my-reports"
 
+// Public Routes
 Route::get('/', function () {
     return Inertia::render('welcome/welcome');
 })->name('home');
@@ -32,11 +33,16 @@ Route::get('/item-details', function () {
     return Inertia::render('item-details');
 })->name('item-details');
 
+// lost items
+Route::resource('lost-items', LostItemController::class);
+
+// Protected Routes
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
