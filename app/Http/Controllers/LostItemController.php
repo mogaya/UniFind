55ebdd4ee82204438d0f928ee1f208a9d37be2f2
mogaya <mogayainnocent@gmail.com>
2025-09-14 23:26:31 +0300

    requre authentication for write routes

--- app/Http/Controllers/LostItemController.php
+++ app/Http/Controllers/LostItemController.php
@@ -10,6 +10,13 @@ use Inertia\Inertia;
 
 class LostItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only([
+            'create', 'store', 'edit', 'update', 'destroy',
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      */
