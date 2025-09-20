dad970316d859dfb43c11cb4350ed3397c220359
mogaya <mogayainnocent@gmail.com>
2025-09-21 00:54:10 +0300

    remove sidebar on settings pages

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -4,7 +4,8 @@ import AppearanceTabs from '@/components/appearance-tabs';
 import HeadingSmall from '@/components/heading-small';
 import { type BreadcrumbItem } from '@/types';
 
-import AppLayout from '@/layouts/app-layout';
+import CustomSection from '@/layouts/custom-section';
+import Layout from '@/layouts/Layout';
 import SettingsLayout from '@/layouts/settings/layout';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -16,15 +17,16 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 export default function Appearance() {
     return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Appearance settings" />
-
-            <SettingsLayout>
-                <div className="space-y-6">
-                    <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
-                    <AppearanceTabs />
-                </div>
-            </SettingsLayout>
-        </AppLayout>
+        <Layout>
+            <CustomSection>
+                <Head title="Appearance settings" />
+                <SettingsLayout>
+                    <div className="space-y-6">
+                        <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
+                        <AppearanceTabs />
+                    </div>
+                </SettingsLayout>
+            </CustomSection>
+        </Layout>
     );
 }
