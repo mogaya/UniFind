197328fa43d569df8f5f057f54ec3e8a5a3a1ffa
mogaya <mogayainnocent@gmail.com>
2025-09-22 17:24:54 +0300

    contact finder dialogue

--- resources/js/pages/found-items/show.tsx
+++ resources/js/pages/found-items/show.tsx
@@ -1,10 +1,20 @@
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
 import CustomSection from '@/layouts/custom-section';
 import Layout from '@/layouts/Layout';
 import { router, usePage } from '@inertiajs/react';
-import { AlertTriangle, ArrowLeft, Calendar, MapPin, MessageCircle, Phone } from 'lucide-react';
+import { AlertTriangle, ArrowLeft, Calendar, Mails, MapPin, MessageCircle, Phone } from 'lucide-react';
 import { toast } from 'sonner';
 
 type ItemProps = {
@@ -35,14 +45,19 @@ const Show = () => {
     };
 
     const handleContactFinder = () => {
-        const contact = item.contact_info || item.user_email;
-        toast('Contact Information', {
-            description: `You can reach ${item.user_name} at: ${contact}`,
-            action: {
-                label: 'Undo',
-                onClick: () => console.log('Undo'),
-            },
-        });
+        <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
+            </DialogHeader>
+
+            <DialogFooter>
+                <DialogClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+            </DialogFooter>
+        </DialogContent>;
     };
 
     const goBack = () => {
@@ -102,10 +117,45 @@ const Show = () => {
                                 <Button variant="default" size="lg" onClick={handleClaimItem} className="w-full">
                                     Claim This Item
                                 </Button>
-                                <Button variant="outline" size="lg" onClick={handleContactFinder} className="w-full">
-                                    <MessageCircle className="mr-2 h-4 w-4" />
-                                    Contact Finder
-                                </Button>
+                                {/* Contact Finder Dialogue */}
+
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                        <Button variant="outline" size="lg" className="w-full">
+                                            <MessageCircle className="mr-2 h-4 w-4" />
+                                            Contact Finder
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="max-w-md">
+                                        <DialogHeader>
+                                            <DialogTitle>Select Channel to contact {item.user_name}</DialogTitle>
+                                            <DialogDescription>You will be redirected to {item.user_name}'s accounts</DialogDescription>
+                                        </DialogHeader>
+                                        <DialogFooter className="mt-5">
+                                            <Button
+                                                variant="outline"
+                                                onClick={() => {
+                                                    const phone = item.contact_info;
+                                                    const message = encodeURIComponent(
+                                                        `I am contacting you from Unifind in regards to an Item you found with the following details: \n\nItem Name: ${item.item_name}\nCategory: ${item.category_name}\nDescription: ${item.description}\n\nSee the image here: ${item.photo_url}`,
+                                                    );
+                                                    window.open(`https://wa.me/+254${phone}?text=${message}`, '_blank');
+                                                }}
+                                            >
+                                                <MessageCircle className="mr-2 h-4 w-4" />
+                                                WhatsApp
+                                            </Button>
+
+                                            <Button variant="outline" onClick={() => window.open(`mailto:${item.user_email}`, '_blank')}>
+                                                <Mails className="mr-2 h-4 w-4" />
+                                                Email
+                                            </Button>
+                                            <DialogClose asChild>
+                                                <Button variant="outline">Cancel</Button>
+                                            </DialogClose>
+                                        </DialogFooter>
+                                    </DialogContent>
+                                </Dialog>
                             </div>
                         </div>
 
