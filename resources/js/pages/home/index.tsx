d1f458f95f1acc8cb57d449149dbc5a2fffa4f26
mogaya <mogayainnocent@gmail.com>
2025-09-27 09:55:41 +0300

    add type prop to ItemCard for recently found items

--- resources/js/pages/home/index.tsx
+++ resources/js/pages/home/index.tsx
@@ -79,7 +79,7 @@ export default function Index() {
 
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                     {recentFoundItems.map((item) => (
-                        <ItemCard key={item.id} {...item} onViewDetails={handleViewDetails} />
+                        <ItemCard type={'found'} key={item.id} {...item} onViewDetails={handleViewDetails} />
                     ))}
                 </div>
             </CustomSection>
