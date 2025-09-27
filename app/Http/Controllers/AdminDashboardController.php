2e2835d7d7c845c04cc64120c26f0191b3801ec9
mogaya <mogayainnocent@gmail.com>
2025-09-27 14:33:22 +0300

    refactor AdminDashboardController to enhance data retrieval for users, lost items, and found items

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,6 +2,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\FoundItem;
+use App\Models\LostItem;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,7 +16,68 @@ class AdminDashboardController extends Controller
     public function index()
     {
         //
-        return Inertia::render("admin-dashboard/index");
+        $lostItemsCount  = LostItem::count();
+        $foundItemsCount = FoundItem::count();
+
+        // users data
+
+        $users = User::withCount(['foundItems', 'lostItems'])->get();
+
+        $userData = $users->map(function ($user) {
+            return [
+                'id'         => $user->id,
+                'name'       => $user->name,
+                'email'      => $user->email,
+                'role'       => $user->role,
+                'found'      => $user->found_items_count,
+                'lost'       => $user->lost_items_count,
+                'created_at' => $user->created_at->toDateString(),
+            ];
+        });
+
+        // lost_items data
+        $lostItems = LostItem::with(['user:id,name,email', 'category:id,category_name'])->get();
+
+        $lostItemsData = $lostItems->map(function ($lostItem) {
+            return [
+                'id'                 => $lostItem->id,
+                'item_name'          => $lostItem->item_name,
+                'category'           => $lostItem->category->category_name,
+                'last_seen_location' => $lostItem->last_seen_location,
+                'user_email'         => $lostItem->user->email ?? 'N/A',
+                'user_name'          => $lostItem->user->name ?? 'N/A',
+                'date'               => $lostItem->created_at->toDateString(),
+                'time'               => $lostItem->created_at->format('h:i A'),
+                'status'             => $lostItem->status,
+
+            ];
+        });
+
+        // found_items data
+        $foundItems = FoundItem::with(['user:id,name,email', 'category:id,category_name'])->get();
+
+        $foundItemsData = $foundItems->map(function ($foundItem) {
+            return [
+                'id'          => $foundItem->id,
+                'item_name'   => $foundItem->item_name,
+                'category'    => $foundItem->category->category_name,
+                'where_found' => $foundItem->where_found,
+                'user_email'  => $foundItem->user->email ?? 'N/A',
+                'user_name'   => $foundItem->user->name ?? 'N/A',
+                'date'        => $foundItem->created_at->toDateString(),
+                'time'        => $foundItem->created_at->format('h:i A'),
+                'status'      => $foundItem->status,
+
+            ];
+        });
+
+        return Inertia::render("admin-dashboard/index", [
+            'lostItemsCount'  => $lostItemsCount,
+            'foundItemsCount' => $foundItemsCount,
+            'users'           => $userData,
+            'lostItems'       => $lostItemsData,
+            'foundItems'      => $foundItemsData,
+        ]);
     }
 
     /**
