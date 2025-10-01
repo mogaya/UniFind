4601ae193150ec8eb75c3026dc62d9ef87b1cd76
mogaya <mogayainnocent@gmail.com>
2025-10-01 20:51:43 +0300

    edit status to only pending and resolved

---
+++ database/migrations/2025_10_01_163741_update_status_enum_in_lost_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lost_items', function (Blueprint $table) {
+            //
+            DB::statement("ALTER TABLE lost_items MODIFY status ENUM('pending','resolved') DEFAULT 'pending'");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lost_items', function (Blueprint $table) {
+            //
+            DB::statement("ALTER TABLE lost_items MODIFY status ENUM('pending','found','resolved') DEFAULT 'pending'");
+
+        });
+    }
+};
